dc547d7d5bd37b21c0d7618da4b2305383e3d861
ogahsergesergehoo <sergeleo14@yahoo.fr>
2026-04-18 21:57:39 +0000

    first commit

---
+++ ProjectFlow/settings/prod.py
@@ -0,0 +1,23 @@
+from .base import *
+
+import os
+
+DEBUG = True
+
+# SECRET_KEY = 'django-insecure-xosdj%%2kmu2yvy5s2ut*r_#+zvp+_tm21h(en$*a0lla-q=4b'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = Path(os.environ.get('STATIC_ROOT', BASE_DIR / 'staticfiles'))
+STATICFILES_DIRS = [BASE_DIR / 'static'] if (BASE_DIR / 'static').exists() else []
